3c277c321897651f6c508b80c8d655ce04c9e20b
Prayudha22 <prayudhamusthofa@gmail.com>
2024-05-18 20:45:57 +0700

    citra

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : Prayudha Ragil Musthofa')
+st.subheader('NIM : 312210726')
+st.subheader('Kelas : TI.22.C6')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/yudha1.jpg", caption="Prayudha", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/ragilprayudha94/")
+st.link_button("Github", "https://github.com/Prayudha22")
